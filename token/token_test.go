8b6fc230bf18e548cbfc983a54e1d10919bab610
PyMarcus <marcus-v@outlook.com>
2026-01-31 12:18:10 -0300

    feat: added identifier validation

--- token/token_test.go
+++ token/token_test.go
@@ -1,1 +1,11 @@
 package token
+
+import "testing"
+
+func TestToken(t *testing.T) {
+	tok := &Token{Type: "string", Literal: "o"}
+	if tok.Type != "string" && tok.Literal != "o" {
+		t.Fatalf("tests - tokentype wrong. expected=%q, got=%q",
+			"string", tok.Type)
+	}
+}
