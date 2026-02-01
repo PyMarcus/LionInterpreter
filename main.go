b3a135bc35ae6ee7324edffd88b954afee4e82ca
PyMarcus <marcus-v@outlook.com>
2026-02-01 15:31:57 -0300

    feat: entrypoint

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/PyMarcus/lioninterpreter/repl"
+)
+
+func main() {
+	user, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Welcome, %s, to Lion Programming Language!\n", user.Username)
+	repl.Start(os.Stdin, os.Stdout)
+}
