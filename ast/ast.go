e90285827d3165ae238cd7ac671498ab5d087aca
PyMarcus <marcus-v@outlook.com>
2026-02-15 12:30:44 -0300

    feat: added ast

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,7 @@
 package ast
 
+import "github.com/PyMarcus/lioninterpreter/token"
+
 // abstract syntax tree
 type Node interface {
 	TokenLiteral() string
@@ -25,3 +27,22 @@ func (p *Program) TokenLiteral() string {
 	}
 	return ""
 }
+
+// representation let x = 5 (token) (name) (value)
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
